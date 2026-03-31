b9f3523938b774523d2a151598d583634c0d29cf
t9327215-prog <t9327215@gmail.com>
2026-03-31 02:21:28 -0300

    refactor: remove App.Flux and update Sistema.Nucleo.Inicializador

--- SistemaFlux/Sistema.Nucleo.Inicializador.tsx
+++ SistemaFlux/Sistema.Nucleo.Inicializador.tsx
@@ -3,26 +3,21 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import SistemaNucleoApp from './Sistema.Nucleo.App';
 import { loadEnvironment } from '../ServiçosFrontend/ValidaçãoDeAmbiente/config.ts';
-import AppFlux from './App.Flux';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-async function inicializarAplicacao() {
-  // A inicialização do serviço de autenticação foi removida daqui
-  // pois será gerenciada pelo AuthProvider.
-  montarNucleoReact();
+function iniciarApp() {
+  try {
+    loadEnvironment();
+    montaReact();
+  } catch (erro) {
+    console.error("Erro crítico durante a inicialização", erro);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  AppFlux.iniciar();
-  inicializarAplicacao();
-});
-
-export function montarNucleoReact() {
-  loadEnvironment();
-
+function montaReact() {
   const rootElement = document.getElementById('root');
   if (!rootElement) {
     throw new Error("Elemento 'root' não foi encontrado.");
@@ -41,3 +36,5 @@ export function montarNucleoReact() {
     </React.StrictMode>
   );
 }
+
+document.addEventListener('DOMContentLoaded', iniciarApp);
